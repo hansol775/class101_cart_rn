9fb80a6eeed00b22aaf0731acd34e1b029cd5fe9
Kim Hansol <46973058+hansol775@users.noreply.github.com>
2020-05-26 15:59:10 +0900

    add user store

---
+++ src/settings/stores/index.js
@@ -0,0 +1,10 @@
+import UserStore from './user';
+
+class RootStore {
+    constructor() {
+        this.userStore = new UserStore(this);
+    }
+}
+
+export default new RootStore()
+
---
+++ src/settings/stores/user.js
@@ -0,0 +1,39 @@
+import { observable, action, computed } from "mobx";
+import AsyncStorage from '@react-native-community/async-storage'
+
+export default class UserStore {
+
+    @observable CartList = [];
+    CartKey = '@CartList';
+
+    constructor(rootStore) {
+        this.rootStore = rootStore;
+        this.fetchCartList();
+    }
+
+    @action
+    async fetchCartList(): void {
+        let res = await AsyncStorage.getItem(this.CartKey) || '[]';
+        this.CartList = JSON.parse(res);
+    }
+
+    @action
+    async addCartList(item) {
+        let lists = [...this.CartList];
+        this.CartList = lists.concat([item]);
+        AsyncStorage.setItem(this.CartKey, JSON.stringify(this.CartList));
+    }
+
+    @computed
+    get CartListIds() {
+        return this.CartList.map(e => e.id)
+    }
+
+    @action
+    async deleteCartList(index) {
+        let lists = [...this.CartList];
+        lists.splice(index, 1);
+        AsyncStorage.setItem(this.CartKey, JSON.stringify(lists));
+        this.CartList = lists;
+    }
+}
